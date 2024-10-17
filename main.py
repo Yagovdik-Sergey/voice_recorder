920d94a2d0ae1c621cf95b75f72cf670ec294a3c
Yagovdik-Sergey <yagovdik.sergey@mail.ru>
2024-10-17 16:09:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import wave
+import os
+import time
+import threading
+import tkinter
+import pyaudio
+
+
+class VoiceRecorder():
+
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.title('Voice Recorder')
+        self.root.geometry('250x200')
+        self.root.resizable(True, False)
+        self.button = tkinter.Button(
+            text="🎙",
+            font=('Cambria', 120, "bold"),
+            command=self.click_handler
+        )
+        self.button.pack()
+        self.label = tkinter.Label(text='00:00:00')
+        self.label.pack()
+        self.recording = False
+        self.root.mainloop()
+
+    def click_handler(self):
+        if self.recording:
+            self.recording = False
+        else:
+            self.recording = True
+            threading.Thread(target=self.record).start()
+
+    def record(self):
+        audio = pyaudio.PyAudio()
+        stream = audio.open(
+            format=pyaudio.paInt16,
+            channels=1,
+            rate=44100,
+            input=True,
+            frames_per_buffer=1024
+        )
+
+        frames = []
+        start = time.time()
+
+        while self.recording:
+            data = stream.read(1024, exception_on_overflow=False)
+            frames.append(data)
+
+            passed = time.time() - start
+            secs = passed % 60
+            mins = passed // 60
+            hours = mins // 60
+            self.label.config(text=f"{int(hours):02d}:{int(mins):02d}:{int(secs):02d}")
+
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+
+        sound_file = wave.open(f'recording{time.ctime()}.wav', 'wb')
+        sound_file.setnchannels(1)
+        sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+        sound_file.setframerate(44100)
+        sound_file.writeframes(b''.join(frames))
+        sound_file.close()
+
+
+VoiceRecorder()
